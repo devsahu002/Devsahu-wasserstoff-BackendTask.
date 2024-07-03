c81d3ff4260f1599be0ac00e9e094c849228512d
devsahu002 <162089107+devsahu002@users.noreply.github.com>
2024-07-03 18:40:29 +0530

    Update loadBalancer.js

--- loadBalancer.js
+++ loadBalancer.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const QueueManager = require('./src/controllers/queueManager');
+const QueueManager = require('./queueManager');
 
 const queueManager = new QueueManager();
 
